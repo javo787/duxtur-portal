c8d95020ba5fdbaff9304e67108a78356511a489
javo787 <user.dr04@gmail.com>
2026-05-14 15:17:42 +0500

    Refactor LangLayout to include SessionProvider

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -3,21 +3,6 @@ import "../globals.css";
 import { SessionProvider } from 'next-auth/react';
 import { auth } from '@/auth';
 
-export default async function LangLayout({ children, params }) {
-  const { lang } = await params;
-  const session = await auth();
-
-  return (
-    <html lang={lang} dir="ltr">
-      <body className={`${inter.variable} ${fraunces.variable} antialiased`}>
-        <SessionProvider session={session}>
-          {children}
-        </SessionProvider>
-      </body>
-    </html>
-  );
-}
-
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin", "latin-ext"],
@@ -41,10 +26,14 @@ export default async function LangLayout({
   params: Promise<{ lang: string }>;
 }) {
   const { lang } = await params;
+  const session = await auth();
+
   return (
     <html lang={lang} dir="ltr">
       <body className={`${inter.variable} ${fraunces.variable} antialiased`}>
-        {children}
+        <SessionProvider session={session}>
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
