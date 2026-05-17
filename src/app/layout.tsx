93cf58ce91ae8f8b26518aa664536d33fc933a27
javo787 <user.dr04@gmail.com>
2026-05-17 19:49:06 +0500

    Remove shortcut icon from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,6 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", rel: "shortcut icon" },
       { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
       { url: "/favicon.svg", type: "image/svg+xml" },
     ],
